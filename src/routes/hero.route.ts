6ddd5f86d1ec386fa10d28159e68504d0c33a764
vansh <vanshpatel1299@gmail.com>
2025-04-30 23:47:36 +0530

    Hero: Comp

--- src/routes/hero.route.ts
+++ src/routes/hero.route.ts
@@ -4,6 +4,60 @@ import { heroData } from '../controllers/hero.controller';
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * 
+ * tags:
+ *   - name: Hero
+ *     description: Fetches hero stock data
+ */
+
+/**
+ * @swagger
+ * /api/hero:
+ *   get:
+ *     summary: Get hero stock data
+ *     tags: [Hero]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Hero stocks fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hero Stocks fetched successfully
+ *                 heroStocks:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: number
+ *                         example: 1
+ *                       ticker:
+ *                         type: string
+ *                         example: SENSEX
+ *                       tickerId:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                           example: SENSEX-SPOT
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 
 router.get('/hero', authenticateToken, heroData);
 
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './swagger';
 import userRouter from './routes/user.route';
 import newsRouter from './routes/news.route';
+import heroRouter from './routes/hero.route';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Routes
 app.use('/api/users', userRouter);
 app.use('/api/', newsRouter);
+app.use('/api/', heroRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
