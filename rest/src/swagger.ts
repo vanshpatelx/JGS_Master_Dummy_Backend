14774268e3e1cbeec68165edbcecfbd59be35735
vansh <vanshpatel1299@gmail.com>
2025-05-10 02:21:30 +0530

    final ws

--- rest/src/swagger.ts
+++ rest/src/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: 'https://jgedemomaster.oceanlab.in',
+        url: 'https://jgedemomaster.oceanlab.in/api/',
       },
       {
         url: 'http://localhost:3000',
